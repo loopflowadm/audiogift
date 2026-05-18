c05d1e1a3d5da7fae263fd979d69e6b3f6c93494
Thacio Maikon <thaciomaikon@gmail.com>
2026-05-18 20:47:47 -0300

    Style improvements: solid background for quiz and white logo inside a black capsule badge

--- src/main.js
+++ src/main.js
@@ -1359,7 +1359,7 @@ const Quiz = () => {
           </div>
           <div class="quiz-top-info">
             <span>Passo ${currentStep + 1} de ${steps.length}</span>
-            <div class="quiz-logo-small">${Logo('var(--black)')}</div>
+            <div class="quiz-logo-small">${Logo('white', 'white')}</div>
             <span>${progress}% Completo</span>
           </div>
         </div>
